61a171b12bc9fd27f0749100bf1b4f487769f5d0
hrswcksono <hariswicaksono250999@gmail.com>
2021-07-29 10:04:19 +0700

    up

--- SanberApp/App.js
+++ SanberApp/App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Telegram from './Tugas/Tugas12/Telegram';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Telegram />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    color:'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
